3d4a785191035617f6b6aa446148c91a4282cc2d
Alex Yudaev <lexx1304@gmail.com>
2024-08-09 13:43:29 +0300

    final tutorial version

--- src/edit.js
+++ src/edit.js
@@ -11,7 +11,8 @@ import { __ } from '@wordpress/i18n';
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, ColorPicker, TextControl, ToggleControl } from '@wordpress/components';
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -29,10 +30,56 @@ import './editor.scss';
  *
  * @return {Element} Element to render.
  */
-export default function Edit() {
+export default function Edit(props) {
+	const {attributes, setAttributes} = props;
+
+	// Extract classes with costom class
+	const {className, ...blockProps} = useBlockProps({
+		className : "hero-video"
+	});
+
+
 	return (
-		<p { ...useBlockProps() }>
-			{ __( 'First Block – hello from the editor!', 'first-block' ) }
-		</p>
+		<>
+			<InspectorControls>
+				<PanelBody title={ __( 'Block Setting', 'first-block' ) }>
+					<TextControl
+						label="Video ID"
+						value={ attributes.videoId }
+						onChange={ ( value ) => { 
+							setAttributes({
+								videoId: value, 
+								videoImage : `https://img.youtube.com/vi/${value}/hqdefault.jpg`
+								}) 
+							}
+						}
+					/>
+					<ToggleControl
+						label={ __( 'Use Shaddow', 'first-block' ) }
+						checked={ attributes.videoShape }
+						onChange={ value => setAttributes( {videoShape: !attributes.videoShape})}
+					/>
+					{
+						attributes.videoShape &&
+						<ColorPicker
+							color={attributes.videoShapeColor}
+							onChange={ value => setAttributes( {videoShapeColor: value})}
+							enableAlpha
+							copyFormat="hex"
+						/>
+					}
+				</PanelBody>
+			</InspectorControls>
+			<section className={className} { ...blockProps }>
+				<div className="iframe-wrapper">
+					{	
+						attributes.videoShape &&
+						<div className="iframe-shape" style={{backgroundColor:attributes.videoShapeColor} }></div>
+					}
+					<img src={attributes.videoImage} width="560" height="315" />
+				</div>
+			</section>
+		</>
+
 	);
 }
--- src/save.js
+++ src/save.js
@@ -15,10 +15,30 @@ import { useBlockProps } from '@wordpress/block-editor';
  *
  * @return {Element} Element to render.
  */
-export default function save() {
+export default function Save(props) {
+
+    const {attributes : {videoId, videoShape, videoShapeColor}} = props;
+    
+    const {className, ...blockProps} = useBlockProps.save({
+        className : "hero-video"
+    });
+
 	return (
-		<p { ...useBlockProps.save() }>
-			{ 'First Block – hello from the saved content!' }
-		</p>
+        <section className={className} { ...blockProps }>
+			<div className="iframe-wrapper">
+				<iframe 
+					width="560" height="315" 
+					src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&enablejsapi=1&loop=1&controls=0&playlist=${videoId}&rel=0`}
+					title="YouTube video player" 
+					frameborder="0" 
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+					allowfullscreen
+					></iframe> 
+                {	
+                    videoShape &&
+                    <div className="iframe-shape" style={{backgroundColor:videoShapeColor} }></div>
+                }
+			</div>
+		</section>
 	);
 }
